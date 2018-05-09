36ce0b85fa7f329f6858acba5eac1c20741a6636
Ryan Berger <ryanbberger@gmail.com>
2018-05-08 21:03:59 -0600

    added 'better' header

--- handlers/header-handler.go
+++ handlers/header-handler.go
@@ -14,12 +14,16 @@ func baseDir(dir string) string  {
 	return baseDir + "/" + dir
 }
 
-func HeaderHandler(dir, name, header string) func(writer http.ResponseWriter, request *http.Request)  {
+type header struct {
+	Title string
+}
+
+func HeaderHandler(dir, name, title string) func(writer http.ResponseWriter, request *http.Request)  {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		t, err := template.ParseFiles(baseDir("templates/header.html"), baseDir(dir))
 		if err != nil{
 			panic(err)
 		}
-		t.ExecuteTemplate(writer, name, "Yeet")
+		t.ExecuteTemplate(writer, name, header{Title: title})
 	}
 }
