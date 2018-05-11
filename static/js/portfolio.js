0648914c584ae3e72b505aebc51be2eaa5d33ca5
Ryan Berger <ryanbberger@gmail.com>
2018-05-10 21:40:52 -0600

    added modal

--- static/js/portfolio.js
+++ static/js/portfolio.js
@@ -5,37 +5,81 @@ function Skill(name, color, imageSrc) {
     this.imageSrc = imageSrc
 }
 
-var android = new Skill("", "", "");
-var go = new Skill("", "", "");
-var postgres = new Skill("", "", "");
-var machineLearning = new Skill("", "", "");
-var web = new Skill("", "", "");
+var android = new Skill("Android", "#2e7d32", "/static/assets/android.png");
+var go = new Skill("Golang", "#1976d2", "");
+var postgres = new Skill("Postgres", "#304ffe", "/static/assets/database.png");
+var machineLearning = new Skill("Machine Learning", "#6200ea", "/static/assets/chart-bar-stacked.png");
+var web = new Skill("Web", "#dd2c00", "/static/assets/web.png");
 
 var projects = {
     vidangel: {
-        img: "",
-        description: "",
+        img: "/static/assets/vidangel.png",
+        description: "Bacon ipsum dolor amet ball tip t-bone kielbasa pancetta burgdoggen, alcatra pork pastrami short loin.",
         awards: [],
-        skills: [android, postgres]
+        skills: [android, postgres],
+        link: ""
     },
     vampir: {
-        img: "",
-        description: "",
+        img: "/static/assets/vampir.png",
+        description: "Bacon ipsum dolor amet ball tip t-bone kielbasa pancetta burgdoggen, alcatra pork pastrami short loin.",
         awards: [""],
-        skills: [android, go, web, postgres, machineLearning]
+        skills: [android, go, web, postgres, machineLearning],
+        link: ""
     }
 };
 
 
+function createSkill(skill) {
+    return $("<img style='background: " + skill.color + "' class='modal__skill' src=" + skill.imageSrc + ">")
+}
+
+
 function fillOutModal(project) {
 
-    $(".modal-container").show()
+    var skills = $(".modal__skills")
+
+    $(".modal-container").css({"display": "flex"});
+
+    $(".modal__image").attr('src', project.img);
+    $(".modal__description").text(project.description);
+    skills.empty();
+
+    for (var i = 0; i < project.skills.length; i++) {
+        skills.append(createSkill(project.skills[i]));
+    }
+}
+
+function lock() {
+    $("body").css({
+        overflow: "hidden",
+        position: "fixed",
+        height: "100vh"
+    });
+}
+
+function unlock() {
+    $("body").css({
+        overflow: "hidden",
+        position: "fixed",
+        height: "auto"
+    });
 }
 
 $(document).ready(function() {
-    $(".project").click(function () {
-        var id = $(this).attr('id');
+
+    $(".modal-container").click(function () {
+        $(this).hide()
+    });
+
+    $(".modal").click(function (e) {
+        e.stopPropagation();
+        unlock()
+    });
+
+    $(".project__about").click(function () {
+        var id = $(this).parent().attr('id');
         fillOutModal(projects[id])
+        lock()
     });
 });
 
