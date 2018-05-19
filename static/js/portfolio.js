16b5f9aae9f0d99fd8c2c183212ed8d1162bd923
Ryan Berger <ryanbberger@gmail.com>
2018-05-18 18:39:54 -0600

    finished product

--- static/js/portfolio.js
+++ static/js/portfolio.js
@@ -17,9 +17,7 @@ var projects = {
         description: "This project was a large Java app written in an MVC like way. After joining the VidAngel Team, " +
         "I've converted 15% of their codebase into Kotlin, and dropped the size of the code base by over 10%. " +
         "Converting the app architecture over to MVVM has been a challenge, but has brought massive benefits with maintainability.",
-        awards: [],
-        skills: [android, postgres],
-        link: ""
+        skills: [android, postgres]
     },
     vampir: {
         img: "/static/assets/vampir.png",
@@ -28,9 +26,25 @@ var projects = {
         "I decided on Machine Learning and entered into a Hackathon. There we took first place, and then went " +
         "on to take first place at BYU's Student Innovator of the year competition. Since then, we have decided to open source" +
         "the project after Google's announcement that they would be using similar technology.",
-        awards: ["Student Innovators of the Year"],
-        skills: [android, web, postgres, machineLearning],
-        link: ""
+        skills: [android, web, postgres, machineLearning]
+    },
+    syds: {
+        img: "/static/assets/s",
+        description: "After watching my sister struggle to maintain her blood sugar for years, " +
+        "I thought that there had to be a better way. After a year of planning and looking over solutions, " +
+        "I decided on Machine Learning and entered into a Hackathon. There we took first place, and then went " +
+        "on to take first place at BYU's Student Innovator of the year competition. Since then, we have decided to open source" +
+        "the project after Google's announcement that they would be using similar technology.",
+        skills: [web]
+    },
+    clearplex: {
+        img: "/static/assets/clearplex.jpg",
+        description: "After watching my sister struggle to maintain her blood sugar for years, " +
+        "I thought that there had to be a better way. After a year of planning and looking over solutions, " +
+        "I decided on Machine Learning and entered into a Hackathon. There we took first place, and then went " +
+        "on to take first place at BYU's Student Innovator of the year competition. Since then, we have decided to open source" +
+        "the project after Google's announcement that they would be using similar technology.",
+        skills: [android, web, postgres]
     }
 };
 
